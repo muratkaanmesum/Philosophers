279ad3d0b998085bb607de0d7b2c37f7329d6da9
MKM327 <kaanmesum@gmail.com>
2023-01-19 17:41:44 +0300

    t

--- main.c
+++ main.c
@@ -3,21 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mmesum <mmesum@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 23:50:55 by mmesum            #+#    #+#             */
-/*   Updated: 2023/01/18 10:39:26 by kali             ###   ########.fr       */
+/*   Updated: 2023/01/19 17:41:44 by mmesum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Philosophers.h"
 
-int	check_if_dead(t_philo *philo)
-{
-	if (get_current_time() - philo->last_eat > philo->data->time_to_die)
-		return (1);
-	return (0);
-}
 void	*philo(void *data)
 {
 	t_philo	*philo;
@@ -28,17 +22,15 @@ void	*philo(void *data)
 	while (!philo->data->is_dead)
 	{
 		eating(philo);
+		pthread_mutex_lock(&philo->data->print);
 		print_message(philo, "Is sleeping");
+		pthread_mutex_unlock(&philo->data->print);
 		smart_sleep(philo->data->time_to_sleep);
+		pthread_mutex_lock(&philo->data->print);
 		print_message(philo, "Is thinking");
-		if (philo->data->must_eat != -1
-			&& philo->eat_count == philo->data->must_eat)
-			return (NULL);
-		if (check_if_dead(philo))
-		{
-			print_message(philo, "Is dead");
-			philo->data->is_dead = 1;
-		}
+		pthread_mutex_unlock(&philo->data->print);
+		if (check_all_cases(philo->data))
+			break ;
 	}
 	return (NULL);
 }
@@ -52,6 +44,11 @@ void	create_threads(t_philo *philos)
 	{
 		pthread_create(&(philos[i].thread), NULL, philo, &philos[i]);
 		i++;
+	}
+	while (1)
+	{
+		if (check_all_cases(philos->data))
+			break ;
 	}
 }
 
