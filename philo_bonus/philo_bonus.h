484e7badd49a93c0e82f249b1a6c10dbf65dcb89
MKM327 <kaanmesum@gmail.com>
2023-01-20 10:09:21 +0300

    t

--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: mmesum <mmesum@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 00:21:56 by mmesum            #+#    #+#             */
-/*   Updated: 2023/01/20 10:01:41 by mmesum           ###   ########.fr       */
+/*   Updated: 2023/01/20 10:04:36 by mmesum           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,16 @@ typedef struct s_data
 	int				must_eat;
 	struct s_philo	*philos;
 	unsigned long	start_time;
-	pthread_mutex_t	print;
-	pthread_mutex_t	eat;
 	int				is_dead;
 }					t_data;
 
 typedef struct s_philo
 {
 	int				id;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
 	int				eat_count;
 	unsigned long	last_eat;
 	int				*is_eating;
 	t_data			*data;
-	pthread_t		thread;
 }					t_philo;
 
 int					ft_atoi(char *str);
@@ -50,10 +45,4 @@ t_data				*init_data(char **argv);
 void				init_mutexes(t_data *data);
 unsigned long		get_current_time(void);
 unsigned long		get_passed_time(unsigned long begin);
-void				print_message(t_philo *philo, char *message);
-void				smart_sleep(int time);
-void				eating(t_philo *philo);
-int					check_all_cases(t_data *data);
-int					check_all_eat(t_data *data);
-int					check_if_dead(t_data *data);
 #endif
